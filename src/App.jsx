39f1796dc1a7c66d17857387e30bb590f618dda4
kwonjieon <holemart@naver.com>
2025-01-25 22:35:25 +0900

    파일구조수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,47 @@
+// import './App.css';
+// import Home from './pages/Home';
+// import Photo from './pages/Photo';
+// import Song from './pages/Song';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import Header from './components/Header';
+
+// function App() {
+//   return (
+//     <>
+//       <Router>
+//         <Header />
+//         <Routes>
+//           <Route path="/" element={<Home />} />
+//           <Route path="/photo" element={<Photo />} />
+//           <Route path="/song" element={<Song />} />
+//         </Routes>
+//       </Router>
+//     </>
+//   );
+// }
+
+// export default App;
 import './App.css';
 import Home from './pages/Home';
 import Photo from './pages/Photo';
 import Song from './pages/Song';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
+import Celebration from './pages/Celebration';
 
 function App() {
   return (
-    <>
+    <div className="container">
       <Router>
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/photo" element={<Photo />} />
           <Route path="/song" element={<Song />} />
+          <Route path="/celebration" element={<Celebration />} />
         </Routes>
       </Router>
-    </>
+    </div>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,33 @@
+// import { Link } from 'react-router-dom';
+
+// export default function Header() {
+//   return (
+//     <div>
+//       <Link to="/">Home</Link>
+//       <Link to="/photo">Photo</Link>
+//       <Link to="/song">Song</Link>
+//     </div>
+//   );
+// }
 import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
-    <div>
-      <Link to="/">Home</Link>
-      <Link to="/photo">Photo</Link>
-      <Link to="/song">Song</Link>
-    </div>
+    <header className="py-4 mb-4">
+      <nav className="flex justify-center space-x-4">
+        <Link to="/" className="btn">
+          Home
+        </Link>
+        <Link to="/photo" className="btn">
+          Photo
+        </Link>
+        <Link to="/song" className="btn">
+          Song
+        </Link>
+        <Link to="/celebration" className="btn">
+          축하영상
+        </Link>
+      </nav>
+    </header>
   );
 }
